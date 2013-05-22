60a5e017624b9fc2925f488080d78f7752f303e6
Jim Fennacy <jim@m29.com>
2013-05-21 17:01:34 -0700

    Testing Commit

---
+++ jftest.php
@@ -0,0 +1,9 @@
+<?php
+
+$years = array(date('Y')-1, date('Y'));
+$months = array(1,2,3,4,5,6,7,8,9,10,11,12);
+foreach ($years as $year) {
+  foreach ($months as $month) {
+    echo "$month, $year\n";
+  }
+}
